90d1485d6c50ce106458b64c76eca4d6b0e19de3
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-01-25 02:49:00 +0530

    deleting post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post  = require('../models/post');
 const db = require('../config/mongoose');
+const Comment = require('../models/comment');
 module.exports.create = function(req, res){
 Post.create ({
    content : req.body.content,
@@ -12,4 +13,19 @@ Post.create ({
     }
     return res.redirect('back');
 });
+}
+
+// deleting post
+module.exports.destroy = function(req, res){
+  Post.findById(req.params.id, function(err, post){
+    // .id means converting the object id into string
+    if(post.user == req.user.id){
+        post.remove();
+      Comment.deleteMany({post:req.params.id}, function(err){
+        return res.redirect('back');
+      })
+    }else{
+      return res.redirect('back');
+    }
+  })
 }
