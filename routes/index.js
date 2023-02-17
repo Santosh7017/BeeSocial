c39ea1a38fce4ef6a7f8c349dfc4e20a1c699fec
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-02-18 00:53:19 +0530

    rendering an api

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const homeController = require('../controllers/home_controller')
 
 
 router.get('/',homeController.home);
-
+   
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+
+router.use('/api', require('./api'));
 //  for any further routes, access from here
 // router.use('/routername',require('./reouterfile'));
 
