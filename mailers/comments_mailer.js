f63ae0c2900b558b6d341ac5f2ca441a5ab18c1b
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-03-08 22:31:49 +0530

    configured to send templateon email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,13 +3,14 @@ const nodemailer = require('../config/nodemailer');
 
 // this is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('inside new comment mailer', comment);    
+    // console.log('inside new comment mailer', comment); 
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs')   
 
     nodemailer.transporter.sendMail({
         from: 'smorya994@gmail.com',
         to: comment.user.email,
         subject: "New comment published",
-        html: '<h1> Yup, your comment is now published! </h1>'
+        html: htmlString
 
     },(err, info)=>{
         if(err){
@@ -18,5 +19,6 @@ exports.newComment = (comment) => {
         }
         console.log('Message sent', info);
         return;
-    })
+    });
+
 }
