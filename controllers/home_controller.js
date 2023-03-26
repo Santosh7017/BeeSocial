068dfd14556d661c664167d1b2a60367089c7b32
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-03-26 20:55:32 +0530

    updates

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,8 @@
-const Post = require('../models/post');
-const User = require('../models/user');
+const Post = require("../models/post");
+const User = require("../models/user");
 // module.exports.actionName = function(req, res){};
 
-module.exports.home = async function(req,res){
+module.exports.home = async function (req, res) {
     // return res.end('<h1>Express is up for beesocial</h1>');
 
     // printing values of cookies
@@ -11,63 +11,60 @@ module.exports.home = async function(req,res){
     // altering the value of cookie
     // res.cookie('user_id',25);
 
-
     // Post.find({},function(err, posts){
     //     return res.render('home',{
     //         title:'BeeSocial | home',
     //         posts: posts
     // });
-   
+
     // });
-        //  Populating the user of each posts
-//     Post.find({})
-//     .populate('user')
-//     .populate({
-//         path: 'comments'
-//     })
-//     .exec(function(err,posts){
-//         return res.render('home',{
-//             title:'BeeSocial | home',
-//             posts: posts
-//         });
-//     });
- // populate the user of each post
+    //  Populating the user of each posts
+    //     Post.find({})
+    //     .populate('user')
+    //     .populate({
+    //         path: 'comments'
+    //     })
+    //     .exec(function(err,posts){
+    //         return res.render('home',{
+    //             title:'BeeSocial | home',
+    //             posts: posts
+    //         });
+    //     });
+    // populate the user of each post
 
-//  tells the server that code contains asynchronous statements you need to wait with each asynchronous statement which have been marked ones one get executed then move to the next statement
-try {
-    
- let posts = await Post.find({})
- .sort('-createdAt')
- .populate('user')
- .populate({
-     path: 'comments',
-     populate: {
-         path: 'user'
-     }
- });
-//  .exec(function(err, posts){
+    //  tells the server that code contains asynchronous statements you need to wait with each asynchronous statement which have been marked ones one get executed then move to the next statement
+    try {
+        let posts = await Post.find({})
+            .sort("-createdAt")
+            .populate("user")
+            .populate({
+                path: "comments",
+                populate: {
+                    path: "user",
+                },
+            });
+        //  .exec(function(err, posts){
 
-//     User.find({},function(err, users){
-//         return res.render('home', {
-//             title: "Beesocial | Home",
-//             posts:  posts,
-//             all_users: users
-//         });
+        //     User.find({},function(err, users){
+        //         return res.render('home', {
+        //             title: "Beesocial | Home",
+        //             posts:  posts,
+        //             all_users: users
+        //         });
 
-//     });
-//  });
+        //     });
+        //  });
 
-let users = await User.find({});
-    return res.render('home', {
-        title: "Beesocial | Home",
-        posts:  posts,
-        all_users: users
-    });
-
-} catch (error) {
-    console.log('Error', error);
-}
-}
+        let users = await User.find({});
+        return res.render("home", {
+            title: "Beesocial | Home",
+            posts: posts,
+            all_users: users,
+        });
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
 
 // this is using then
 // Post.find({}).populate('comments').then(function());
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,17 +1,17 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = async function(req, res){
-    try{
+module.exports.create = async function (req, res) {
+    try {
         let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-        
-        if (req.xhr){
+
+        if (req.xhr) {
             // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
-                // type of ajax req is xml http request(XHR)
-            post = await post.populate('user');
+            // type of ajax req is xml http request(XHR)
+            post = await post.populate('user', 'name');
 
             return res.status(200).json({
                 data: {
@@ -24,13 +24,13 @@ module.exports.create = async function(req, res){
         req.flash('success', 'Post published!');
         return res.redirect('back');
 
-    }catch(err){
+    } catch (err) {
         req.flash('error', err);
         // added this to view the error on console as well
         console.log(err);
         return res.redirect('back');
     }
-  
+
 }
 // deleting post
 // module.exports.destroy = function(req, res){
@@ -50,18 +50,18 @@ module.exports.create = async function(req, res){
 // deleting post
 // Converting into async
 
-module.exports.destroy = async function(req, res){
+module.exports.destroy = async function (req, res) {
 
-    try{
+    try {
         let post = await Post.findById(req.params.id);
- // .id means converting the object id into string
-        if (post.user == req.user.id){
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
             post.remove();
 
-            await Comment.deleteMany({post: req.params.id});
+            await Comment.deleteMany({ post: req.params.id });
 
 
-            if (req.xhr){
+            if (req.xhr) {
                 return res.status(200).json({
                     data: {
                         post_id: req.params.id
@@ -73,14 +73,14 @@ module.exports.destroy = async function(req, res){
             req.flash('success', 'Post and associated comments deleted!');
 
             return res.redirect('back');
-        }else{
+        } else {
             req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
 
-    }catch(err){
+    } catch (err) {
         req.flash('error', err);
         return res.redirect('back');
     }
-    
+
 }
