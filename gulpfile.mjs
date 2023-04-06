afd5a792d9c8a9d3ccf0af99006536e5d3d9e204
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-04-07 00:58:01 +0530

    small changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -57,21 +57,55 @@ const sass = gulpSass(dartSass);
 import cssnano from 'gulp-cssnano';
 import rev from 'gulp-rev';
 
-export const css = () => {
+// export const css = async () => {
+//   console.log("minifying css....");
+//     gulp.src('./assets/sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+    
+//     .pipe(gulp.dest('./assets/css'))
+
+//     console.log("Adding hash value");
+//      gulp.src('./assets/**/*.css')
+//     .pipe(rev())
+//     .pipe(gulp.dest('./public/assets'))
+//     .pipe(rev.manifest({
+//       cwd: 'public',
+//       merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets'));
+//     console.log("Done");
+// };
+
+// gulp.task('default', css);
+
+gulp.task('css' ,  function(done){//css is the task name 
+    
   console.log("minifying css....");
-  return  gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets/css'))
+  gulp.src('./assets/scss/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))//converts sass to css//pipe is used to call additional middleware for gulp
+  .pipe(cssnano())//this will minify the css
+  .pipe(gulp.dest('./assets/css'))//the destination of finally minimised files
 
+  // gulp.src('./assets/**/*.css')//we take the files from the main assets 
+  // .pipe(rev())//hash the names so that if a browser has a assets file with the name in the cache it doesnt ignore ours
+  // .pipe(gulp.dest('./public/assets'))//and the file with the hashed names has stored here
+  // .pipe(rev.manifest({//manifest is the file whch =conatins the key falue pairs witth originals and hashed file names - { page1.css: page1-xys3993#ma.css , chatEngine.css : chatEngine920bnd2910#2.css}
+  //     cwd:'public',//current working directory
+  //     merge :true
+  // }))
+  // .pipe(gulp.dest('./public/assets'));
+  done();
+})
+gulp.task('rev', function(done){
+  console.log("Adding hash value");
    gulp.src('./assets/**/*.css')
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-      cwd: 'public',
-      merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-};
-
-gulp.task('default', css);
+  .pipe(rev())
+  .pipe(gulp.dest('./public/assets'))
+  .pipe(rev.manifest({
+    cwd: 'public',
+    merge: true
+  }))
+  .pipe(gulp.dest('./public/assets'));
+  console.log("Done");
+  done();
+});
