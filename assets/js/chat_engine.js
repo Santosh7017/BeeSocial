66cdda30f7992434b3c0d12d79c966ca9160dbe9
Santosh Kumar Morya <santoshmorya400@gmail.com>
2023-06-03 22:05:38 +0530

    formatted some document

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,23 +1,23 @@
-class chatEngine{
+class chatEngine {
 
-    constructor(chatBoxId , userEmail){
+    constructor(chatBoxId, userEmail) {
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000', { transport : ['websocket'] });
+        this.socket = io.connect('http://localhost:5000', { transport: ['websocket'] });
 
-        if(this.userEmail){
+        if (this.userEmail) {
             this.connectionHandler();
         }
     }
 
-    createMessagePill(data){
+    createMessagePill(data) {
 
         let senderMail = data.user_email;
         let msg = data.msg
         console.log("create");
-        let messageType = 'other-message' ;
-        if(senderMail === this.userEmail){
+        let messageType = 'other-message';
+        if (senderMail === this.userEmail) {
             messageType = 'self-message';
         }
 
@@ -30,50 +30,50 @@ class chatEngine{
 
     }
 
-    connectionHandler(){
+    connectionHandler() {
 
         let self = this;
 
         //1this will bascially fire an even to establish our conection
-        this.socket.on('connect' , function(){
+        this.socket.on('connect', function () {
             console.log("connection using sockets !");
-            
+
             //1)what we do is that we emit an event('join_room') from the front end which will tell the backend that a user wants to join a vhat room
             //along with the emit we send some data 
             //this req will be recived at the baackdend chat_socket
-            self.socket.emit('join_room' , {
-                user_email : self.userEmail,
-                chat_room : 'codial'
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chat_room: 'codial'
             });
 
             //4)we confiem if a user has joned the room
-            self.socket.on('user_joined' , function(data){
-                console.log("user joined " , data);
+            self.socket.on('user_joined', function (data) {
+                console.log("user joined ", data);
             })
 
             //whenver the send messga button is clicked and even is fired which send the messga e to the server
-            $('#send-message').click(function(){
+            $('#send-message').click(function () {
 
                 let message = $('#chat-message-input').val();
 
-                if(message != ''){
+                if (message != '') {
 
-                    self.socket.emit('send_message' , {
-                        user_email : self.userEmail,
-                        chat_room : 'codial',
-                        msg : message
+                    self.socket.emit('send_message', {
+                        user_email: self.userEmail,
+                        chat_room: 'codial',
+                        msg: message
                     });
                     $('#chat-message-input').val("");
-                   
+
                 }
 
 
             })
 
             //we create an event detected on to recieve messages 
-            self.socket.on('receive_message' , function(data){
+            self.socket.on('receive_message', function (data) {
 
-                console.log('message recved !' , data.msg);
+                console.log('message recved !', data.msg);
                 let messagePill = self.createMessagePill(data);
                 $(".chat-messages-list").append(messagePill);
 
--- routes/users.js
+++ routes/users.js
@@ -6,37 +6,38 @@ const userController = require('../controllers/users_controller');
 const postController = require('../controllers/posts_controller');
 
 
-router.get('/profile/:id',passport.checkAuthentication,userController.profile);  // this will redirect to authentication page 
-router.post('/update/:id',passport.checkAuthentication,userController.update);
+router.get('/profile/:id', passport.checkAuthentication, userController.profile);  // this will redirect to authentication page 
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 
 // router.get('/posts',postController.posts);
-router.get('/login',userController.login);
-router.get('/register',userController.register);
-router.post('/create',userController.create);
-router.get('/forgotpassword',userController.forgotpassword);
-router.post('/verifytoken',userController.verifyToken);
-router.get('/resetpassword',userController.resetPassword);
+router.get('/login', userController.login);
+router.get('/register', userController.register);
+router.post('/create', userController.create);
+router.get('/forgotpassword', userController.forgotpassword);
+router.post('/verifytoken', userController.verifyToken);
+router.get('/resetpassword', userController.resetPassword);
 router.post('/updatePass', userController.updatePass);
 
 // use passport as a middleware to authenticate
-router.post('/login',passport.authenticate(
+router.post('/login', passport.authenticate(
     'local',
-    {failureRedirect:'/users/login',
-    // successRedirect: '/'
-}),
-// ,function(req,res)
-// {
-// //   todo s- fix the problem of redirecting
-//     console.log("login succesfull");
-//     res.end('<h1>Logged in succesfully please go to home page <a href="/">Home</a></h1>');
-// }
-userController.createSession
+    {
+        failureRedirect: '/users/login',
+        // successRedirect: '/'
+    }),
+    // ,function(req,res)
+    // {
+    // //   todo s- fix the problem of redirecting
+    //     console.log("login succesfull");
+    //     res.end('<h1>Logged in succesfully please go to home page <a href="/">Home</a></h1>');
+    // }
+    userController.createSession
 );
 
-router.get('/logout',userController.destroySession);
+router.get('/logout', userController.destroySession);
 
-router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
-router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/login'}),userController.createSession);
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/users/login' }), userController.createSession);
 
 module.exports = router;
 
